019d6b199614aeabd89055bbf09ab9e8dc9441ab
Max Luciani Pereira <max_luciani@hotmail.com>
2020-09-17 21:11:23 -0300

    criando um novo repositorio

---
+++ ExerciciosTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace entra_21_tests_lista_2
+{
+    public class ExerciciosTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
