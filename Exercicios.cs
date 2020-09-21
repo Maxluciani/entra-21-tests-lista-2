c62bd2b5934005b86d81d40e4ee8bf13793b9ecd
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-09-21 19:21:37 -0300

    alterações

--- Exercicios.cs
+++ Exercicios.cs
@@ -6,9 +6,10 @@ namespace entra_21_tests_lista_2
     public class Exercicios
     {
         [Fact]
-        public void Test1()
+        public int Exercicio01()
         {
-
+           // Ler um array com 10 inteiros e mostrar os números na ordem direta e 
+             //inversa a que foram lidos.
         }
     }
 }
