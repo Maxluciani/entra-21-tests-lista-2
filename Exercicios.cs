a153718ec2dbfc5a6be1c9d26fbe63c4ee3b0ca2
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-09-21 21:08:45 -0300

    exercicio02

--- Exercicios.cs
+++ Exercicios.cs
@@ -6,10 +6,17 @@ namespace entra_21_tests_lista_2
     public class Exercicios
     {
         [Fact]
-        public int Exercicio01()
+        public int[] Exercicio02(int[] array10)
         {
            // Ler um array com 10 inteiros e mostrar os números na ordem direta e 
              //inversa a que foram lidos.
+
+             var resultado = new int [10];
+             for (int i = 0; i < array10.Length; i++)
+             {
+                 resultado[array10.Length - i] = array10[i];
+             }
+             return resultado;
         }
     }
 }
--- ExerciciosTests.cs
+++ ExerciciosTests.cs
@@ -5,10 +5,16 @@ namespace entra_21_tests_lista_2
 {
     public class ExerciciosTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(new int[10]{10,20,30,40,50,60,70,80,90,100},new int [10]{100,90,80,70,60,50,40,30,20,10})]
+        public void Ordem_direta_e_indireta_de_um_array(int[] array10 , int[] expectativa)
         {
-
+            var exercicio = new Exercicios();
+            var resultado = exercicio.Exercicio02(array10);
+            for (int i = 0; i < expectativa.Length; i++)
+            {
+                Assert.Equal(expectativa[i],resultado[i]);
+            }
         }
     }
 }
