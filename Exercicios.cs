f949ef98d7e4bb8d392df8ca2c0876b1f82a47dc
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-09-22 19:41:31 -0300

    mais testes da primiera lista de arrays

--- Exercicios.cs
+++ Exercicios.cs
@@ -3,6 +3,17 @@ namespace entra_21_tests_lista_2
     public class Exercicios
     {
         
+        public double[] Exercicio01(double[] arraya,double[] arrayb)
+        {
+            //Leia dois arrays A e B com 15 elementos. Construir um array C, onde cada 
+           //elemento de C é a subtração do elemento correspondente de A com B.
+           var resultado = new double [10];
+            for (int i = 0; i < arrayb.Length; i++)
+            {
+                resultado[i] = arraya[i] - arrayb[i];
+            }
+            return resultado;
+        }
         public int[] Exercicio02(int[] array10)
         {
            // Ler um array com 10 inteiros e mostrar os números na ordem direta e 
@@ -14,17 +25,6 @@ namespace entra_21_tests_lista_2
                  resultado[array10.Length - (i + 1)] = array10[i];
              }
              return resultado;
-        }
-        public double[] Exercicio01(double[] arraya,double[] arrayb)
-        {
-            //Leia dois arrays A e B com 15 elementos. Construir um array C, onde cada 
-           //elemento de C é a subtração do elemento correspondente de A com B.
-           var resultado = new double [10];
-            for (int i = 0; i < arrayb.Length; i++)
-            {
-                resultado[i] = arraya[i] - arrayb[i];
-            }
-            return resultado;
         }
     }
 }
--- ExerciciosTests.cs
+++ ExerciciosTests.cs
@@ -6,17 +6,6 @@ namespace entra_21_tests_lista_2
     public class ExerciciosTests
     {
         [Theory]
-        [InlineData(new int[10]{10,20,30,40,50,60,70,80,90,100},new int [10]{100,90,80,70,60,50,40,30,20,10})]
-        public void Ordem_direta_e_indireta_de_um_array(int[] array10 , int[] expectativa)
-        {
-            var exercicio = new Exercicios();
-            var resultado = exercicio.Exercicio02(array10);
-            for (int i = 0; i < expectativa.Length; i++)
-            {
-                 Assert.Equal(expectativa[i],resultado[i]);
-            }
-        }
-        [Theory]
         [InlineData(new double[10]{2,4,6,8,10,12,14,16,18,20},new double[10]{1,2,3,4,5,6,7,8,9,10},
         new double[10]{1,2,3,4,5,6,7,8,9,10})]
         public void array_c(double[] arraya,double[] arrayb,double[] arrayc)
@@ -27,6 +16,17 @@ namespace entra_21_tests_lista_2
          {
              Assert.Equal(arrayc[i],resultado[i]);
          }
+        }
+        [Theory]
+        [InlineData(new int[10]{10,20,30,40,50,60,70,80,90,100},new int [10]{100,90,80,70,60,50,40,30,20,10})]
+        public void Ordem_direta_e_indireta_de_um_array(int[] array10 , int[] expectativa)
+        {
+            var exercicio = new Exercicios();
+            var resultado = exercicio.Exercicio02(array10);
+            for (int i = 0; i < expectativa.Length; i++)
+            {
+                 Assert.Equal(expectativa[i],resultado[i]);
+            }
         }
 
     }
